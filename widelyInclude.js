f32b3a5893ea0558074b1b78b23e7f5a304b5c36
shashwatg07 <shashwatgrjr616@gmail.com>
2017-06-06 14:36:01 +0530

    Update widelyInclude.js

--- widelyInclude.js
+++ widelyInclude.js
@@ -1,9 +1,4 @@
-		var _config = {};
-		_config.service_worker_url = './service-worker.js'; 
-		_config.hashId = '<YOUR HASHID>'; 
-        _config.service_worker_scope = '/pwa-ecommerce/';
-        _config.views = {};
-        _config.views.notification = {};
-        _config.views.notification.position = 'bottom-left';
-		Widely.pwa(_config); 
-	
+		<script src="https://widely.global.ssl.fastly.net/lib/1.0.6/widely.js"> </script> 
+<script type="text/javascript"> 
+! function() {function e(e) { if (4 == i.readyState && 200 == i.status) { var o = i.responseText,a = {}; "null" != o ? (a = JSON.parse(o), localStorage.setItem("widely::widelyConfig", JSON.stringify(a))) : window.localStorage && void 0 != localStorage.getItem("widely::widelyConfig") ? a = JSON.parse(localStorage.getItem("widely::widelyConfig")) : (a.service_worker_url = "./service-worker.js", a.hashId = t), Widely.pwa(a) }}var t = "-KlrfpwEUnS6auUuw-LF",i = new XMLHttpRequest, o = "https://progresshive.firebaseio.com/users/"+t+"/pwaMappings/customize.json"; i.open("GET", o, !0), i.send(), i.addEventListener("readystatechange", e, !1) }(); 
+</script> 
